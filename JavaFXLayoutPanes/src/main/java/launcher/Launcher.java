6781bbab81e76fdd492d4fdccb14a85e57275cd0
Josh Archer <jarcher@greenriver.edu>
2017-10-10 12:06:51 -0700

    Added examples for using StackPane and BorderPane layouts.

--- JavaFXLayoutPanes/src/main/java/launcher/Launcher.java
+++ JavaFXLayoutPanes/src/main/java/launcher/Launcher.java
@@ -1,12 +1,14 @@
 package launcher;
 
 import javafx.application.Application;
+import layouts.BorderPanePractice;
 import layouts.GridLayoutPractice;
+import layouts.StackPanePractice;
 
 public class Launcher
 {
     public static void main(String[] args)
     {
-        Application.launch(GridLayoutPractice.class, args);
+        Application.launch(BorderPanePractice.class, args);
     }
 }
---
+++ JavaFXLayoutPanes/src/main/java/layouts/BorderPanePractice.java
@@ -0,0 +1,63 @@
+package layouts;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class BorderPanePractice extends Application
+{
+    @Override
+    public void start(Stage stage)
+    {
+        stage.setTitle("Practicing with BorderPane");
+        stage.setScene(getScene());
+        stage.show();
+    }
+
+    private Scene getScene()
+    {
+        BorderPane parentPanel = new BorderPane();
+
+        //buttons to the top
+        HBox topPanel = new HBox(); //nested panel
+        topPanel.setAlignment(Pos.CENTER);
+        parentPanel.setTop(topPanel);
+
+        topPanel.getChildren().addAll(new Button("Click"),
+                new Button("Me"), new Button("Now!"));
+
+        //text on the left
+        VBox leftPanel = new VBox();
+        parentPanel.setLeft(leftPanel);
+
+        String[] sayings = {"If I don't push the limits!!!",
+                "Lasagna, lasagna, lasagna...", "Don't let your dreams be dreams!",
+                "Some people dream of greatness, others work hard for it!"};
+
+        for (int i = 0; i < sayings.length; i++)
+        {
+            leftPanel.getChildren().add(getTextControl(sayings[i]));
+        }
+
+        //list on the right
+
+        //image in the middle
+
+        //paragraph on the bottom
+
+        return new Scene(parentPanel, 400, 400);
+    }
+
+    private Text getTextControl(String message)
+    {
+        Text control = new Text(message);
+        control.setWrappingWidth(120);
+        return control;
+    }
+}
